ae16cb6c2dd9837f18986e03c045bb09953dd183
rink1969 <rink1969@cryptape.com>
2017-06-30 03:52:03 +0800

    wrapper for sha3

--- util/sha3/src/lib.rs
+++ util/sha3/src/lib.rs
@@ -2,3 +2,16 @@ extern {
 	pub fn sha3_256(out: *mut u8, outlen: usize, input: *const u8, inputlen: usize) -> i32;
 	pub fn sha3_512(out: *mut u8, outlen: usize, input: *const u8, inputlen: usize) -> i32;
 }
+
+extern crate tiny_keccak;
+extern crate rand;
+extern crate rustc_serialize;
+extern crate bigint;
+extern crate libc;
+#[macro_use]
+extern crate heapsize;
+
+
+pub mod sha3;
+pub mod hash;
+
