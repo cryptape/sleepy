d8c7ef13238be19a8fa7353db78b6e313233d92c
rink1969 <rink1969@cryptape.com>
2017-07-02 07:31:19 +0800

    add admintool

---
+++ admintool/admintool.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+display_help()
+{
+	echo 
+	echo "usage: $0 -l ip_list -d block_duration -h HZ"
+	echo "option:"
+	echo "-l ip_list     list all the node's IP and port"
+	echo "    default value is '127.0.0.1:4000,127.0.0.1:4001,127.0.0.1:4002,127.0.0.1:4003'"
+	echo
+	echo "-d block_duration    block generating duration(second)"
+	echo "    default value is '6'"
+	echo
+	echo "-h HZ    times try to generate block per second"
+	echo "    default value is '10'"
+	echo
+	exit 0
+}
+
+# parse options
+while getopts 'a:p:l:n:m:d:tb:f:' OPT; do
+    case $OPT in
+        l)
+            IP_LIST="$OPTARG";;
+        d)
+            DURATION="$OPTARG";;
+        h)
+            HZ="$OPTARG";;
+        ?)
+            display_help
+    esac
+done
+
+#set default value
+if [ ! -n "$IP_LIST" ]; then
+	DEV_MOD=1
+    IP_LIST="127.0.0.1:4000,127.0.0.1:4001,127.0.0.1:4002,127.0.0.1:4003"
+fi
+
+if [ ! -n "$DURATION" ]; then
+    DURATION=6
+fi
+
+if [ ! -n "$HZ" ]; then
+    HZ=10
+fi
+
+#calc size of nodes
+TMP=${IP_LIST//[^\:]}
+SIZE=${#TMP}
+
+DATA_PATH=`pwd`/release
+
+rm -rf $DATA_PATH
+
+if [ ! -f "$DATA_PATH" ]; then
+    mkdir -p $DATA_PATH
+fi
+
+for ((ID=0;ID<$SIZE;ID++))
+do
+	mkdir -p $DATA_PATH/node$ID
+	echo "Start generating private Key for Node" $ID "!"
+	python create_keys_addr.py $DATA_PATH $ID
+	echo "[PrivateKey Path] : " $DATA_PATH/node$ID
+	echo "End generating private Key for Node" $ID "!"
+done
+
+for ((ID=0;ID<$SIZE;ID++))
+do
+	echo "Start creating Network Node" $ID "Configuration!"
+	python create_config.py $DATA_PATH $ID $SIZE $IP_LIST $DURATION $HZ
+	echo "End creating Network Node" $ID "Configuration!"
+	echo "########################################################"
+done
+
+for ((ID=0;ID<$SIZE;ID++))
+do
+    echo "Start creating Node " $ID " env!"
+	echo "Start copy binary and migrations for Node " $ID "!"
+	cp -rf $DATA_PATH/../target/debug/sleepy $DATA_PATH/node$ID/
+done
+echo "********************************************************"
+echo "WARN: remember then delete all privkey files!!!"
---
+++ admintool/create_config.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import copy
+import sys
+import math
+
+def make_config():
+	nid = int(sys.argv[2])
+	path = os.path.join(sys.argv[1],"node" + str(nid))
+	ip_list = (sys.argv[4]).split(',')
+	duration = int(sys.argv[5])
+	hz = int(sys.argv[6])
+	port = ip_list[nid].split(':')[1]
+	config_name = "config"
+	size = int(sys.argv[3])
+	dump_path = os.path.join(path, config_name)
+	f = open(dump_path, "w")
+	f.write("id_card = " + str(nid) + "\n")
+	f.write("port = " + port + "\n")
+	f.write("max_peer = " + str(size - 1) + "\n")
+	f.write("duration = " + str(duration) + "\n")
+	f.write("hz = " + str(hz) + "\n")
+	secret_path = os.path.join(path, "miner_privkey")
+	secret_key = open(secret_path, "r")
+	key = secret_key.read()
+	f.write("miner_private_key = " + key + "\n")
+	secret_path = os.path.join(path, "signer_privkey")
+	secret_key = open(secret_path, "r")
+	key = secret_key.read()
+	secret_key.close()
+	f.write("signer_private_key = " + key + "\n")
+	ids=range(size)
+	ip_list = zip(ids, ip_list)
+	del ip_list[nid]
+	for (id, addr) in ip_list :
+		addr_list = addr.split(':')
+		f.write("[[peers]]" + "\n")
+		f.write("id_card = " + str(id) + "\n")
+		ip = addr_list[0]
+		f.write("ip = \"" + ip + "\"\n")
+		port = addr_list[1]
+		f.write("port = " + port + "\n")
+	
+    #generate keypairs
+	signer_auth_path = os.path.join(sys.argv[1], "signer_authorities")
+	signer_auth = open(signer_auth_path, "r")
+	miner_auth_path = os.path.join(sys.argv[1], "miner_authorities")
+	miner_auth = open(miner_auth_path, "r")
+
+	while True:
+		signer_key = signer_auth.readline()
+		miner_key = miner_auth.readline()
+		if (not signer_key) or (not miner_key):
+			break
+		f.write("[[keygroups]]" + "\n")
+		f.write("miner_public_key = " + miner_key)
+		f.write("signer_public_key = " + signer_key)
+
+	signer_auth.close()
+	miner_auth.close()
+	f.close()
+
+make_config()
