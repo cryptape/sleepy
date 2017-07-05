c4a8ef38febed6ab5f22bea4e62ac1b75f0a42d6
Ke Wang <kw@cryptape.com>
2017-07-05 16:48:02 -0400

    add kvdb and versioned data structure based on avl merkle tree

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -8,13 +8,28 @@ extern crate rand;
 extern crate tiny_keccak;
 extern crate bigint;
 extern crate time;
+extern crate parking_lot;
+extern crate rlp;
+extern crate elastic_array;
+extern crate heapsize;
+extern crate itertools;
+
+#[macro_use]
+extern crate log as rlog;
 
 pub mod error;
 pub mod config;
+pub mod hashdb;
+pub mod common;
+pub mod memorydb;
+pub mod bytes;
+pub mod standard;
+pub mod sha3;
+pub mod avl;
 
-mod sha3;
-
-pub use sha3::*;
+pub use common::*;
+pub use hashdb::*;
+pub use memorydb::MemoryDB;
 
 pub fn timestamp_now() -> u64 {
     let now = time::now().to_timespec();
